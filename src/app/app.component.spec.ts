e7bcb6a672db523227d4c2cc2f10157588805b6b
flgx <fraan.mp@gmail.com>
2018-02-02 13:01:57 -0300

    added

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,7 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+import {} from 'jasmine';
 describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
